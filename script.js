76c6e4a6aac9d30240318c8da30f5e7b8d6c695b
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-04 11:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+const colorSelect = document.getElementById('colorSelect');
+const button = document.querySelector('input[type="button"]');
+
+/*function color('click', input) {
+	if (color == input) {
+		alert(color.remove());
+	}
+}*/
+
+button.addEventListener('click', function(){
+	let selectedColor = colorSelect.options[colorSelect.selectedIndex].text;
+	for (let i = 0; i < colorSelect.options.length; i++) {
+		if (colorSelect.options[i].text === selectedColor) {
+			colorSelect.remove(i);
+			alert(`Removed color: ${selectedColor}`);
+			break;
+		}
+	}
+});
